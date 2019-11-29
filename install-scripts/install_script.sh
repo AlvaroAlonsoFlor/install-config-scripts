5140eedc985cf3c2dff2421a03f1dfa8c5ad23e8
VladmirVolgen <alvaroalonsoflor@gmail.com>
2019-11-29 18:35:21 +0000

    Add java and jabba install to install script

---
+++ install-scripts/install_script.sh
@@ -0,0 +1,17 @@
+## Install node.js ###
+
+sudo apt install nodejs
+
+### Install Java and Java version manager ###
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+
+##Install java 8 ##
+
+jabba install adopt@1.8.0-232
+
+
+
+
+
+
+### Install Python ###
