2d48f52be9809d3982f4706f0f30f0f7d46db648
VladmirVolgen <alvaroalonsoflor@gmail.com>
2019-11-30 09:38:40 +0000

    Add python installation

--- languages/install_languages.sh
+++ languages/install_languages.sh
@@ -5,13 +5,11 @@ sudo apt install nodejs
 ### Install Java and Java version manager ###
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
 
-##Install java 8 ##
-
 jabba install adopt@1.8.0-232
 
-
-
-
-
-
 ### Install Python ###
+
+sudo apt install python3
+sudo apt install python
+
+
