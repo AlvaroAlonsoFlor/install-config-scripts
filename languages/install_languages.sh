701ed8c33d989e8d7bd697cff2865813791996bd
VladmirVolgen <alvaroalonsoflor@gmail.com>
2019-12-07 18:17:38 +0000

    Fix scripts for installs and terminal after finding issues

--- languages/install_languages.sh
+++ languages/install_languages.sh
@@ -1,6 +1,13 @@
-## Install node.js ###
+## Update Linux subsystem
 
-sudo apt install nodejs
+sudo apt-get update
+sudo apt-get upgrade
+
+## Install node.js ###
+sudo apt-get install build-essential
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+echo source ~/.nvm/nvm.sh > ~/.zshrc
+
 
 ### Install Java and Java version manager ###
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
--- terminal-shell/install-zsh-and-configure.sh
+++ terminal-shell/install-zsh-and-configure.sh
@@ -6,9 +6,37 @@ sudo apt-get install zsh
 
 sudo apt-get install fonts-powerline
 
+## If it does not find the package run this
+#################
+### This script requires remote-signed permissions for powershell ###
+## run as administrator powershell and run: Set-ExecutionPolicy RemoteSigned
+###############
+## Back in the unix shell
+## clone
+#git clone https://github.com/powerline/fonts.git --depth=1
+# install
+#cd fonts
+#./install.sh
+# clean-up a bit
+#cd ..
+
+## Install fonts in windows
+#powershell.exe
+#cd fonts
+#.\install.ps1
+#cd ..
+#exit
+
+#rm -rf fonts
+#######################################
+
 ### Add POWERLEVEL10K to the list of themes in ZSH ###
 
+echo $ZSH_CUSTOM
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
 TERMINAL_CONFIG_FOLDER=$(pwd)
 USER_FOLDER="/mnt/c/Users/$USER"
