49172d51a5fb6bceae2d6d19df54a69ca4a60c7f
VladmirVolgen <alvaroalonsoflor@gmail.com>
2020-05-02 10:57:23 +0100

    Change profile.json to settings.json to comply with latest windows terminal changes

--- languages/install_languages.sh
+++ languages/install_languages.sh
@@ -1,6 +1,18 @@
+
+## Get essentials tools ##
+
+sudo apt-get install build-essential
+
 ## Install node.js ###
 
-sudo apt install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | zsh
+exec $SHELL
+nvm install --lts
+nvm use --lts
+
+## sudo apt install nodejs -- ONLY IF node version manager does not work ## 
+
+
 
 ### Install Java and Java version manager ###
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
