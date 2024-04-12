3cf5e167ea3db8f728dfb1538444e329e02a01ff
Alvaro <alvaroalonsoflor@gmail.com>
2024-04-12 16:22:47 +0100

    fix: wsl script works uses usr bash and points to mount directly

--- espanso/override-config-wsl-to-windows.sh
+++ espanso/override-config-wsl-to-windows.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
 # Override base file
-base_file_path="/c/users/$(whoami)/AppData/Roaming/espanso/match"
+base_file_path="/mnt/c/users/$(whoami)/AppData/Roaming/espanso/match"
 echo "Copying base.yml to $base_file_path"
 cp base.yml "$base_file_path"
 echo "Done"
